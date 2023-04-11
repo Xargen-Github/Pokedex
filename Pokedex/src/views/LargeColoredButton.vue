5d5dcae6867a7d75ce3c1271eee3d0e957d9f96a
Xargen-Github <vanherbergenx@gmail.com>
2023-04-11 11:58:00 +0200

    Added LargeColoredButton to be used in SearchView

---
+++ Pokedex/src/views/LargeColoredButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    
+    export default defineComponent({
+    })
+</script>
+
+<template>
+    <button class="h-24 p-2 rounded-lg text-left flex flex-col justify-end box-border">
+        <span class="text-white font-bold">
+            <slot name="title"></slot>
+        </span>
+        <span class="text-white text-opacity-60">
+            <slot name="subTitle"></slot>
+        </span>
+    </button>
+</template>
